d86148a069f342d1b8c1a9e78c36f64afb6b14e2
David Pan <dpan5425@gmail.com>
2020-09-07 14:59:28 -0400

    added logic to loop through question bank and display questions and answers

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
 var timerEl = document.getElementById("timer");
-var startQuizEl = document.getElementById("start-quiz");
+var startQuizButton = document.getElementById("start-quiz");
 var mainContentEl = document.getElementById("main-content");
 
+var currentQuestionIndex = 0;
+var score = 0;
 
+// Question and answer bank
 var questions = [
-    ["Commonly used data types DO NOT include:", "strings", "booleans", "alerts", "numbers"]
+    {
+      question: "Commonly used data types do NOT include:",
+      answers: [
+        { text: "strings", correct: false },
+        { text: "numbers", correct: false },
+        { text: "booleans", correct: false },
+        { text: "alerts", correct: true },
+      ],
+    },
+    {
+      question:
+        "The condition in an if/else statement is contained within _____?",
+      answers: [
+        { text: "()", correct: true },
+        { text: "%%", correct: false },
+        { text: "<>", correct: false },
+        { text: "{}", correct: false },
+      ],
+    }
 ];
 
 var secondsLeft = 10;
-// Set Timer
+// Set Timer function
 function setTimer() {
   var timerInterval = setInterval(function() {
     secondsLeft--;
@@ -21,21 +42,46 @@ function setTimer() {
   }, 1000);
 }
 
-startQuizEl.addEventListener("click", function(event) {
+startQuizButton.addEventListener("click", function(event) {
     event.preventDefault();
     setTimer();
+    renderQuestion();
+});
 
-    document.getElementById("main-content").innerHTML = "";
-    for(var i = 0; i<questions.length; i++) {
-        for(var j = 0; j<questions[i].length; i++) {
-            var headerEl = document.createElement("h1");
-            headerEl.textContent = questions[i][j];      
-            mainContentEl.append(headerEl1);
+function renderQuestion() {
+    mainContentEl.innerHTML = "";
 
+    // Render question
+    var questionEl = document.createElement("h2");
+    questionEl.textContent = questions[currentQuestionIndex].question;
+    mainContentEl.append(questionEl);
 
+    // Render list of answers
+    var ul = document.createElement("ul");
+    mainContentEl.append(ul);
+
+    // Loops through question bank
+    for(var i = 0; i < questions[currentQuestionIndex].answers.length; i++) {
+
+        var button = document.createElement("button");
+        button.classList.add("button");
+        if (questions[currentQuestionIndex].answers[i].correct) {
+          button.setAttribute("correct", true);
         }
+        button.textContent = questions[currentQuestionIndex].answers[i].text;
+
+        button.addEventListener("click", buttonClick);
+  
+        mainContentEl.appendChild(button);
 
     }
 
-})
+    currentQuestionIndex++;
+}
 
+function buttonClick(event) {
+    if(event.target.matches("button") && event.target.getAttribute("correct") === "true") {
+        score += 100;
+    }
+    renderQuestion();
+}
